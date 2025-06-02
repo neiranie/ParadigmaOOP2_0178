3c32c72356596ffdac2af59cd67de47510c89a5c
neiranie <nurkhairanianneira@gmail.com>
2025-06-02 15:01:08 +0700

    code fix

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -14,5 +14,16 @@ public:
         y = b;
     }
     // menampilkan nilai 
-    void display() {}
+    void display() {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
+
+int main() {
+    AbstraksiKlas ak;
+    ak.setXY("Yogyakarta", "Kampus");
+    ak.display();
+
+    return 0;
 }
